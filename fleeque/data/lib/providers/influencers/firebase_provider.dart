8d82ff142719152cd6ceee30aacd34bc7b7458b9
Bulka <Bulka@MacBook-Pro-6.local>
2023-09-16 18:04:27 +0200

    add filtering stream to repository

--- fleeque/data/lib/providers/influencers/firebase_provider.dart
+++ fleeque/data/lib/providers/influencers/firebase_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:core/filters/filters_constants.dart';
 import 'package:data/model/influencers.dart/influencers_model.dart';
 import 'package:domain/entities/influencers/influencers_entities.dart';
 import 'package:domain/entities/filter/filter_entities.dart';
@@ -30,59 +31,58 @@ class FirebaseProvider {
     return stream;
   }
 
-  void filterInfluencersCollection(FilterEntity filterValues) {}
-
-  // Future<void> filterData(Emitter emit, Map<String, dynamic> filters) async {
-  //   var collectionCopy = [...collection];
-  //   if (filters['time'] == timeNewest) {
-  //     collectionCopy.sort(
-  //         (a, b) => DateTime.parse(a.time).compareTo(DateTime.parse(b.time)));
-  //   } else if (filters['time'] == timeOldest) {
-  //     collectionCopy.sort(
-  //         (b, a) => DateTime.parse(a.time).compareTo(DateTime.parse(b.time)));
-  //   } else if (filters['price'] == priceLowToHigh) {
-  //     collectionCopy.sort((a, b) => a.basicPrice.compareTo(b.basicPrice));
-  //   } else if (filters['price'] == priceHighToLow) {
-  //     collectionCopy.sort((b, a) => a.basicPrice.compareTo(b.basicPrice));
-  //   }
-  //   if (filters['search'] != '') {
-  //     collectionCopy = collectionCopy
-  //         .where(
-  //           (el) => el.name.toLowerCase().contains(
-  //                 filters['search'].toLowerCase(),
-  //               ),
-  //         )
-  //         .toList();
-  //   }
-  //   if (filters['followers'] == followersOverMillion) {
-  //     collectionCopy = collectionCopy
-  //         .where(
-  //           (el) => el.followers > 999999,
-  //         )
-  //         .toList();
-  //   }
-  //   if (filters['followers'] == followersUnderMillion) {
-  //     collectionCopy = collectionCopy
-  //         .where(
-  //           (el) => el.followers < 1000000,
-  //         )
-  //         .toList();
-  //   }
-  //   if (filters['country'] != countrySelect) {
-  //     collectionCopy = collectionCopy
-  //         .where(
-  //           (el) => el.country.toLowerCase().contains(
-  //                 filters['country'].toLowerCase(),
-  //               ),
-  //         )
-  //         .toList();
-  //   }
-  //   emit(
-  //     InfluencersState(
-  //       influencersCollection: collectionCopy,
-  //       isLoading: false,
-  //       isError: false,
-  //     ),
-  //   );
-  // }
+  void filterInfluencersCollection(FilterEntity filterValues) {
+    final influencersCollectionStream = getInfluencersRef();
+    influencersCollectionStream.listen((event) {
+      var list = event.docs
+          .map((docSnap) => InfluencerModel.fromSnapshot(docSnap))
+          .toList();
+      if (filterValues.timeSorting == timeNewest) {
+        list.sort(
+            (a, b) => DateTime.parse(a.time).compareTo(DateTime.parse(b.time)));
+      } else if (filterValues.timeSorting == timeOldest) {
+        list.sort(
+            (b, a) => DateTime.parse(a.time).compareTo(DateTime.parse(b.time)));
+      } else if (filterValues.priceSorting == priceLowToHigh) {
+        list.sort((a, b) => a.basicPrice.compareTo(b.basicPrice));
+      } else if (filterValues.priceSorting == priceHighToLow) {
+        list.sort((b, a) => a.basicPrice.compareTo(b.basicPrice));
+      }
+      if (filterValues.searchText != '') {
+        list = list
+            .where(
+              (el) => el.name.toLowerCase().contains(
+                    filterValues.searchText.toLowerCase(),
+                  ),
+            )
+            .toList();
+      }
+      if (filterValues.followersFiltering == followersOverMillion) {
+        list = list
+            .where(
+              (el) => el.followers > 999999,
+            )
+            .toList();
+      }
+      if (filterValues.followersFiltering == followersUnderMillion) {
+        list = list
+            .where(
+              (el) => el.followers < 1000000,
+            )
+            .toList();
+      }
+      if (filterValues.countryFiltering != countrySelect) {
+        list = list
+            .where(
+              (el) => el.country.toLowerCase().contains(
+                    filterValues.countryFiltering.toLowerCase(),
+                  ),
+            )
+            .toList();
+      }
+      controller.add(
+        list,
+      );
+    });
+  }
 }
